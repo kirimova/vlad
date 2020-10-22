aec8bcbded57141f4c4e2cc5562602508b21140a
kirimova <Vladronaldos35>
2020-10-22 17:37:48 +0300

    C++
    C++

---
+++ C++/1_2_9.cpp
@@ -0,0 +1,20 @@
+﻿// 1.2.9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    double s, a, h, r,R;
+    a = 4;
+    h = (a * sqrt(3) / 2);
+    s = (a * a * sqrt(3) / 4);
+    r = (a * sqrt(3) / 6);
+    R = 2 * r;
+    std::cout << "h=" << h;
+    std::cin >> h;
+    return 0;
+
+    
+}
+
