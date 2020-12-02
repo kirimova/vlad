76b1fd4eb24b3a66cf31a623dee2b86acae9f575
kirimova <vladronaldos05@gmail.com>
2020-12-02 19:39:29 +0300

    C++ V7
    C++ V7

---
+++ C++/2.1/2.1/2.1.cpp
@@ -0,0 +1,19 @@
+﻿// 2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+
+    a *= (a >= 0) ? a : (a*a*a);
+    b *= (b >= 0) ? b : (b*b*b);
+    c *= (c >= 0) ? c : (c*c*c);
+
+    std::cout << a << " " << b << " " << c << endl;
+    system("pause");
+    return 0;
+}
+
---
+++ L5 архетектура аппаратных средств/2.1 C++/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+
+    a *= (a >= 0) ? a : pow(a, 3);
+    b *= (b >= 0) ? b : pow(b, 3);
+    c *= (c >= 0) ? c : pow(c, 3);
+
+    std::cout << a << " " << b << " " << c;
+}
+
